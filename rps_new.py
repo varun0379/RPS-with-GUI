a423621d9b3b0af3c09811a79ed01b2ca245b78c
varun0379 <varun0379@gmail.com>
2021-12-28 22:26:20 +0530

    Functional
    Game is completely functional.
    Reset button has not been implemented. Round counter has not been implemented either

--- rps_new.py
+++ rps_new.py
@@ -17,7 +17,7 @@ def hide(widget): # the widget in the parameter
 # tkinter window
 root = tk.Tk()
 root.title('Rock, Paper, Scissors Game')
-root.geometry('750x600+100+100')
+root.geometry('750x400+400+200')
 root.attributes('-topmost', 1)
 root.resizable(False,False)
 
@@ -51,36 +51,31 @@ reset_btn.grid(column = 2, row = 5, padx = 10, pady = 10)
 # user score text
 yourScore_label = tk.Label(
     root, 
-    text = 'Your Score: ' + str(yourScore) + '\n Your Choice: ' + yourChoice,
+    text = 'Your Score: ' + str(yourScore) + '\n Your Last Choice: ' + yourChoice,
     font = ("Times New Roman",12))
 yourScore_label.grid(column = 0, row = 2, columnspan = 2)
 
 # comp score text
 compScore_label = tk.Label(
     root, 
-    text = ('Computer Score: ' + str(compScore) + '\n Computer Choice: ' + compChoice),
+    text = ("Computer Score: " + str(compScore) + "\n Computer's Last Choice: " + compChoice),
     font = ("Times New Roman",12))
 compScore_label.grid(column = 3, row = 2, columnspan = 2)
 
 def result(yourChoice,compChoice):
     global compScore, yourScore, counter
     if (compChoice == "Rock" and yourChoice == "Scissors") or (compChoice == "Paper" and yourChoice == "Rock") or (compChoice =="Scissors" and yourChoice == "Paper"):
-        print("You chose " + yourChoice + " and the computer chose " + compChoice + ". " + compChoice + " beats " + yourChoice + " so, the computer won.")
-        showinfo("Computer Wins", "You chose " + yourChoice + " and the computer chose " + compChoice + ". " + compChoice + " beats " + yourChoice + " so, the computer won.")
         compScore += 1
         counter += 1
     elif (compChoice == "Scissors" and yourChoice == "Rock") or (compChoice == "Rock" and yourChoice == "Paper") or (compChoice =="Paper" and yourChoice == "Scissors"):
-        print("You chose " + yourChoice + " and the computer chose " + compChoice + ". " + yourChoice + " beats " + compChoice + " so, you won.")
-        showinfo("You win", "You chose " + yourChoice + " and the computer chose " + compChoice + ". " + yourChoice + " beats " + compChoice + " so, you won.")
         yourScore += 1
         counter += 1
     elif compChoice == yourChoice:
-        print("Tie! Nobody gains a point because both you and the computer chose " + yourChoice + ".")
-        showinfo("Tie", "Tie! Nobody gains a point because both you and the computer chose " + yourChoice + ".")
+        showinfo("Tie!", "Nobody gains a point because both you and the computer chose " + yourChoice + ".")
 
 def updateScoreboard():
-    compScore_label['text'] = 'Computer Score: ' + str(compScore) + '\n Computer Choice: ' + compChoice
-    yourScore_label['text'] = 'Your Score: ' + str(yourScore) + '\n Your Choice: ' + yourChoice
+    compScore_label['text'] = "Computer Score: " + str(compScore) + "\n Computer's Last Choice: " + compChoice
+    yourScore_label['text'] = "Your Score: " + str(yourScore) + "\n Your Last Choice: " + yourChoice
 
 def rock():
     global yourChoice, compChoice
@@ -94,18 +89,21 @@ def paper():
     compChoice = choices[random.randint(0,2)]
     yourChoice = "Paper"
     result(yourChoice,compChoice)
+    updateScoreboard()
+
 def scissors():
     global yourChoice, compChoice
     compChoice = choices[random.randint(0,2)]
     yourChoice = "Scissors"
     result(yourChoice,compChoice)
+    updateScoreboard()
 
 #rock button
 rock_img = tk.PhotoImage(file = r"RPS gui/rock.png")
 rock_btn = tk.Button(
         root, 
         image = rock_img, 
-        #command = rock
+        command = rock
         )
 rock_btn.grid(column = 0, row = 3, rowspan = 1,padx = 10)
 
@@ -114,7 +112,7 @@ paper_img = tk.PhotoImage(file = r"RPS gui/paper.png")
 paper_btn = tk.Button(
         root, 
         image = paper_img,
-        #command = paper
+        command = paper
         )
 paper_btn.grid(column = 2, row = 3, rowspan = 1, padx = 10)
 
@@ -123,7 +121,7 @@ scissors_img = tk.PhotoImage(file = r"RPS gui/scissors.png")
 scissors_btn = tk.Button(
         root, 
         image = scissors_img, 
-        #command = scissors
+        command = scissors
         )
 scissors_btn.grid(column = 4, row = 3, rowspan = 1, padx = 10)
 
